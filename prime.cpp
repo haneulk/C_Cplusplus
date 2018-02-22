3a823464c92339413b589a6cf28b9d6d6c603a37
haneulk <hnkim362@naver.com>
2018-02-22 22:38:19 +0900

    소수 구하는 코드 (2~10000까지)

---
+++ prime.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+using namespace std;
+int main() {
+	bool prime[10001] = {};
+
+	for (int i = 2; i <= 10000; i++) {
+		if (prime[i] == false) {
+			for (int j = i*i; j <= 10000; j += i) {
+				prime[j] = true;
+			}
+		}
+	}
+	for (int i = 0; i <= 10000; i++) {
+		prime[i] = !prime[i];
+	}
+	for (int i = 2; i <= 10000; i++) {
+		if (prime[i] == true)
+			printf("%d\n", i); 
+	}
+	return 0;
+}
